a33b131ffc1ea2c978678d6cb3d75b80bbe7c9d4
grzesiupia <52804048+grzesiupia@users.noreply.github.com>
2021-03-27 07:36:20 -0700

    Models updated

--- Backend/DjangoBackend/BackendApi/models.py
+++ Backend/DjangoBackend/BackendApi/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 # Create your models here.
-"""
-class User(models.Model):
-    email = models.CharField(max_length=100)
-    uname = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    class Meta:
-        db_table = "users"
 
-"""
+class Users(models.Model):
+    login = models.CharField(primary_key=True, max_length=255)
+    password = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'users'
