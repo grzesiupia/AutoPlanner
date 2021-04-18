8cb5406dd0b91f5997ba17b23d720f7c7c4ee16f
Grzegorz Piątkowski <grzegorz.piatkowski@hotmail.com>
2021-04-18 17:13:45 +0200

    Schedule filled with random lessons

--- Algorithm_Prototype/Algorithm_py/main.py
+++ Algorithm_Prototype/Algorithm_py/main.py
@@ -4,8 +4,8 @@ IB = {
     "j.polski": [4, "Zbigniew Zbigniewski", ""],
     "biologia": [2, "Bogdan Bogdanowicz", "200"],
     "chemia": [1, "Marianna Mania", "201"],
-    "wf": [4, "Włodzimierz Włodzimski", "Sala gimnastyczna"],
-    "religia": [2, "Ksiądz Robak", ""]
+    "wf": [4, "Wlodzimierz Wlodzimski", "Sala gimnastyczna"],
+    "religia": [2, "Ksiadz Robak", ""]
 }
 
 IA = {
@@ -14,31 +14,39 @@ IA = {
     "j.polski": [4, "Barbara Barbarowicz", ""],
     "biologia": [2, "Bogdan Bogdanowicz", "200"],
     "chemia": [1, "Marianna Mania", "201"],
-    "wf": [4, "Włodzimierz Włodzimski", "Sala gimnastyczna"],
-    "religia": [2, "Ksiądz Robak", ""]
+    "wf": [4, "Wlodzimierz Wlodzimski", "Sala gimnastyczna"],
+    "religia": [2, "Ksiadz Robak", ""]
 }
 
 ToughSubjects = ("matematyka", "fizyka", "j.polski", "biologia", "chemia")
 
 
 class SchoolClass:
-    def __init__(self, dictionary, lessons_per_week):
+    def __init__(self, name_of_class, dictionary, lessons_per_week):
+        self.name = name_of_class
         self.max_lessons_per_day = int(lessons_per_week / 5 + 1)
         self.min_lessons_per_day = int(lessons_per_week / 5 - 1)
         self.max_tough_subjects_per_day = int(sum([dictionary[x][0] for x in dictionary if x in ToughSubjects]) / 5) + 1
         self.subjects = dictionary
 
 
-def make_schedule(list_of_school_classes: SchoolClass, schedule):
-    for school_class_object in list_of_school_classes:
-        for day in schedule:
-            for lesson in school_class_object.subjects:
-                while school_class_object.subjects[lesson][0] > 0:
-                    day.append([lesson, school_class_object.subjects[lesson]])
-                    school_class_object.subjects[lesson][0] -= 1
-    print(schedule)
-
+def make_schedule(list_of_school_classes, schedule):
+    for day in schedule:
+        for h in range(6):
+            day.append([])
+            for school_class_object in list_of_school_classes:
+                for lesson in school_class_object.subjects:
+                    if school_class_object.subjects[lesson][0] > 0:
+                        day[h].append([school_class_object.name, lesson, school_class_object.subjects[lesson]])
+                        school_class_object.subjects[lesson][0] -= 1
+                        break
     return schedule
+
+
+def print_schedule(schedule):
+    f = open('output.json', 'w')
+    f.write(json.dumps(schedule))
+    f.close()
 
 
 if __name__ == "__main__":
@@ -54,4 +62,5 @@ if __name__ == "__main__":
     for l in list_of_received_school_classes:
         list_of_school_class_objects.append(SchoolClass(l, sum([l[x][0] for x in l])))
 
-    make_schedule(list_of_school_class_objects, schedule_for_week)
+    print_schedule(make_schedule(list_of_school_class_objects, schedule_for_week))
+
