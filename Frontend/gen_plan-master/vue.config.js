885205331eef40e36b8b1b4e1401126d931bf2b8
Grzegorz Piątkowski <grzegorz.piatkowski@hotmail.com>
2021-10-25 20:28:52 +0200

    add https false flag

--- Frontend/gen_plan-master/vue.config.js
+++ Frontend/gen_plan-master/vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     devServer: {
-      public: 'http://ec2-35-159-51-127.eu-central-1.compute.amazonaws.com:8080/'
+      public: 'http://ec2-35-159-51-127.eu-central-1.compute.amazonaws.com:8080/',
+      https: false
     }
   };
