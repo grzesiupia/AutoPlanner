c412d098f936e0858393faf152422723796470e4
Grzegorz Piątkowski <grzegorz.piatkowski@hotmail.com>
2021-10-25 20:30:28 +0200

    reverse https flag

--- Frontend/gen_plan-master/vue.config.js
+++ Frontend/gen_plan-master/vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     devServer: {
-      public: 'http://ec2-35-159-51-127.eu-central-1.compute.amazonaws.com:8080/',
-      https: false
+      public: 'http://ec2-35-159-51-127.eu-central-1.compute.amazonaws.com:8080/'
     }
   };
