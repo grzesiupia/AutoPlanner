0008a68b2636b01cd49a793bcbd50aa5a3a9c387
Maciej Goral <maciej636107@gmail.com>
2021-10-25 18:55:04 +0200

    http

--- Frontend/gen_plan-master/src/axios.js
+++ Frontend/gen_plan-master/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const URL = "https://18.185.102.150:8000/" //URL serwera
+export const URL = "http://18.185.102.150:8000/" //URL serwera
 
 export default axios.create({
     baseURL: URL
