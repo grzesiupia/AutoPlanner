14187c24a7220d5d4808ae687d3f05bc6960592e
Grzegorz Piątkowski <grzegorz.piatkowski@hotmail.com>
2021-10-25 18:00:34 +0200

    ec2 backend address added

--- Frontend/gen_plan-master/src/axios.js
+++ Frontend/gen_plan-master/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const URL = "http://127.0.0.1:8000/" //URL serwera
+export const URL = "http://18.185.102.150:8000/" //URL serwera
 
 export default axios.create({
     baseURL: URL
