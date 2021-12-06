a02b4c575062c4c7672d4050def4f96d4a69e3e3
Grzegorz Piątkowski <grzegorz.piatkowski@hotmail.com>
2021-12-06 19:00:12 +0100

    update backend url

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const URL = "http://127.0.0.1:8000/" //URL serwera
+export const URL = "http://3.68.195.5:8000/" //URL serwera
 
 export default axios.create({
     baseURL: URL
