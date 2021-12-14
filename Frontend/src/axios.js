7ef993b25512d39251150a1c1ef5a3995c79e732
grzesiupia <52804048+grzesiupia@users.noreply.github.com>
2021-12-14 22:06:48 +0100

    change ip of server

--- Frontend/src/axios.js
+++ Frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const URL = "http://3.68.195.5:8000/" //URL serwera
+export const URL = "http://3.120.208.16:8000/" //URL serwera
 
 export default axios.create({
     baseURL: URL
