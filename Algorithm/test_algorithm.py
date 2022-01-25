02713fec96002685ad01844c49d99dbb75f8157c
gpiatkow <grzegorz.piatkowski@intel.com>
2022-01-25 08:58:29 +0100

    add pytest tests of algorithm

--- Algorithm/test_algorithm.py
+++ Algorithm/test_algorithm.py
@@ -5,6 +5,7 @@ from .algorithm import Population
 
 
 def test_evolution(groups_data=GROUP_OLD, teachers_data=TEACHERS_OLD, classrooms_data=CLASSES_OLD):
+
     population_size = 10
     num_of_generations = 100
     num_of_mutations = 20
