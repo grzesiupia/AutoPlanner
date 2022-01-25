551f49b64325a5c5c5f05d744e85314d41e1dd1b
grzesiupia <52804048+grzesiupia@users.noreply.github.com>
2022-01-25 19:09:58 +0100

    Add classrooms and teacher preferred hours to evaluation (#43)
    * add classrooms and add preferred teacher hours to evaluation
    
    * fix tests
    
    * fix
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * lets see
    
    * siema
    
    * we will see
    
    * algo init file update
    
    Co-authored-by: gpiatkow <grzegorz.piatkowski@intel.com>

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 # pylint: disable=C0103
+import sys
+sys.path.append(".")
+sys.path.append(".\\Backend")
+sys.path.append(".\\Backend\\backend_api")
+sys.path.append(".\\Backend\\django_backend")
+sys.path.append(".\\Algorithm")
